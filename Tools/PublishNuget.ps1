dc6d47132420a1ab73f8a6e24da5f4689cff0663
discosultan <jaanusvarus@gmail.com>
2017-03-24 19:51:25 +0200

    Update nuspec config

--- Tools/PublishNuget.ps1
+++ Tools/PublishNuget.ps1
@@ -1,5 +1,6 @@
 # Define variables.
-$id = 'MonoGame.Penumbra.WindowsDX'
+$idWindowsDX = 'MonoGame.Penumbra.WindowsDX'
+$idDesktopGL = 'MonoGame.Penumbra.DesktopGL'
 
 $nuspecSuffix = '.nuspec'
 $nupkgSuffix = '.nupkg'
@@ -25,12 +26,15 @@ Function GetNupkgFilename($id)
 
 # Replace version numbers in nuspec files.
 Write-Host Replacing version numbers
-UpdateVersionInFile (GetNuspecFilename $id) $versionRegex $newVersion
+UpdateVersionInFile (GetNuspecFilename $idWindowsDX) $versionRegex $newVersion
+UpdateVersionInFile (GetNuspecFilename $idDesktopGL) $versionRegex $newVersion
 
 # Create nuget packages with symbol packages.
 Write-Host Creating packages
-.\nuget pack (GetNuspecFilename $id) -symbols
+.\nuget pack (GetNuspecFilename $idWindowsDX) -symbols
+.\nuget pack (GetNuspecFilename $idDesktopGL) -symbols
 
 # Publish nuget packages to nuget.org and symbol packages to symbolsource.org
 Write-Host Publishing packages
-.\nuget push (GetNupkgFilename $id) -Source https://www.nuget.org/api/v2/package
+.\nuget push (GetNupkgFilename $idWindowsDX) -Source https://www.nuget.org/api/v2/package
+.\nuget push (GetNupkgFilename $idDesktopGL) -Source https://www.nuget.org/api/v2/package
